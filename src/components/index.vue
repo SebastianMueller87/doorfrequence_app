8e6252c40fcebea302428d78b21d711ac54d3348
Sebastian Müller <sebastian@sourceboat.com>
2018-03-03 18:26:51 +0100

    add moment and add property to filter opening by hour

--- src/components/index.vue
+++ src/components/index.vue
@@ -3,20 +3,27 @@
     .header.text-center
       .col-12
         h1 Index.vue
+      //- ul
+      //-   li(v-bind:key="index" v-for="(item, index) in data")
+      //-     span {{ item.timestamp }} : {{ item.value }}
       ul
-        li(v-bind:key="index" v-for="(entry, index) in data")
-          span {{index}} : {{entry}}
+        li(v-bind:key="index" v-for="(item, index) in openingPerHour")
+          span {{ index }} : {{ item }}
 </template>
 
 <script>
 const axios = require('axios')
+const moment = require('moment')
 
 export default {
   props: {},
 
   data () {
     return {
-      data: {}
+      data: [
+        'test',
+        'test2'
+      ]
     }
   },
 
@@ -33,9 +40,9 @@ export default {
     fetchData () {
       axios.get(`${this.baseUrl}:${this.port}/${this.filename}`)
         .then((response) => {
-            console.log(response.data)
-            console.log(Object.keys(response.data).length)
-            this.data = response.data
+            this.data = Object.keys(response.data).map((key) => {
+              return { timestamp: key, value: response.data[key] }
+            })
         })
         .catch((error) => {
           console.error('Error: ', error)
@@ -43,7 +50,36 @@ export default {
     }
   },
 
-  computed: {}
+  computed: {
+    openingData: function () {
+      return this.data.filter((item, index) => {
+        return item.value === 0
+      })
+    },
+    closingData: function () {
+      return this.data.filter((item, index) => {
+        return item.value === 1
+      })
+    },
+    openingPerHour: function () {
+      let data = []
+      this.openingData.forEach((item, index) => {
+        let date = moment.unix(item.timestamp / 1000)
+
+        if (date.hour() == 6) {
+          console.log(date.format('DD.MM.YYYY hh:mm:ss'))
+        }
+
+        if (!data[date.hour()]) {
+          data[date.hour()] = 1
+        } else {
+          data[date.hour()]++
+        }
+      })
+
+      return data
+    }
+  }
 }
 
 </script>
